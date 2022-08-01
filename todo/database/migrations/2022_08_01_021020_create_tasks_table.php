4a277f195acb96ae2d1908e02efc4c068848fff5
TakumiKuwamoto <109050030+TakumiKuwamoto@users.noreply.github.com>
2022-08-01 11:33:18 +0900

    タスク作成の準備

---
+++ todo/database/migrations/2022_08_01_021020_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->char('title',30);
+            $table->char('contents',140);
+            $table->text('image_at');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
